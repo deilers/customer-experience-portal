65324d93bd1544f24e3bddb1c214424d2fe6fc25
Devon Eilers <devon.d.eilers@gmail.com>
2020-06-28 20:30:09 -0700

    chore: fix lint findings

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase/app';
 // Add the Firebase services that you want to use
-import "firebase/auth";
+import 'firebase/auth';
 import { environment } from '../../environments/environment.prod';
 
 @Injectable({
@@ -14,7 +14,7 @@ export class AuthService {
 
   constructor() { }
 
-  public firebaseSignin(email, password) {
+  public firebaseSignin(email, password): void {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((data) => {
       console.log(`user signup success: ${data.user.email}`);
@@ -22,9 +22,9 @@ export class AuthService {
     .catch((error) => {
       console.error(error);
     });
-  };
+  }
 
-  public initializeApp() {
+  public initializeApp(): void {
     firebase.initializeApp(environment);
   }
 
